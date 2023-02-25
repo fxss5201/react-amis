39266719a5c144c6c8de3964c37572a4b2c683f4
fxss <723107234@qq.com>
2023-02-25 22:20:45 +0800

    feat: 菜单支持自动选中及展开

--- src/layout/components/AsideMenu.jsx
+++ src/layout/components/AsideMenu.jsx
@@ -3,8 +3,10 @@ import { routerList } from "../../router";
 import { useSelector } from 'react-redux';
 import cloneDeep from 'lodash.clonedeep';
 import { searchShowMenuRoutes, showMenuRoutesToMenuItems, showMenuRoutesFilterAccess } from '../../utils/router';
+import { useLocation } from "react-router-dom";
 
 const AsideMenu = () => {
+  const location = useLocation();
 	const userInfo = useSelector(state => state.userInfo.value);
   let sideMenuList = searchShowMenuRoutes(routerList[0].children);
   sideMenuList = sideMenuList[0]?.children;
@@ -12,11 +14,22 @@ const AsideMenu = () => {
 	const sideMenuListFilterAccess = showMenuRoutesFilterAccess(sideMenuListClone, userInfo.access);
 	const sideMenuItems = showMenuRoutesToMenuItems(sideMenuListFilterAccess);
 
+  const activeAndUnfolded = (list) => {
+    return list.map(x => {
+      if (x.children) x.children = activeAndUnfolded(x.children)
+      return {
+        ...x,
+        active: location.pathname === x.path,
+        unfolded: location.pathname.includes(x.path)
+      }
+    })
+  }
+
   const asideMenuSchema = {
     "type": "nav",
     "stacked": true,
     "className": "w-md",
-    "links": sideMenuItems
+    "links": activeAndUnfolded(sideMenuItems)
   }
 
   return <AmisComponent schema={asideMenuSchema} />;
